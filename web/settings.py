1b55d6f48e6d3c0bc2a2e8233dbc74eecd1adddb
Electric Sheep <electricsheepindream@gmail.com>
2017-05-11 19:49:44 +0800

    delete

--- web/settings.py
+++ web/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@ia3453^$-34%9a+=3v-h=9sing-axr3g3e-_os$+-z@muoh(a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
